5d4a90f501c67ade73efe80c4cae37b9faa10e0f
akash.nair <akash.nair@understand.ai>
2023-02-06 20:36:24 +0530

    Working docker image

---
+++ src/test/kotlin/com/up42/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.up42.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
